6c12953071c3104cbc1789ab16bac929d81b3c5e
Nicolas Polhamus <polhamus.n@outlook.com>
2015-04-27 01:17:31 -0400

    CPSC 5175U Programming Project
    This project was to create a server and client that sent a file 10,000
    times, using both TCP and UDP. I was only able to have a file sent once,
    but I know understand network programming in C# better.

---
+++ CPSC 5175U/ProgrammingProject/TCP_UDP_Client.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace _5175U_ProgrammingProject
+{
+    public enum Mode { TCP, UDP };
+    public partial class NetworksProject : Form
+    {
+        TcpClient tClient;
+        UdpClient uClient;
+        public Mode mode;
+        public NetworksProject()
+        {
+            InitializeComponent();
+            mode = Mode.TCP;
+        }
+
+        private void tCPModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mode = Mode.TCP;
+        }
+
+        private void uDPModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mode = Mode.UDP;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            byte[] statusMessageBytes = new byte[256];
+            String message;
+            String filePath = fileLocationBox.Text;
+            statusBox.Clear();
+            if(mode == Mode.TCP)
+            {
+                String ipAddress = serverAddressBox.Text;
+                Int32 port = Convert.ToInt32(portBox.Text);
+                IPAddress address = IPAddress.Parse(ipAddress);
+                IPEndPoint endPoint = new IPEndPoint(address, port);
+                tClient = new TcpClient(ipAddress, port);
+                NetworkStream tStream = tClient.GetStream();
+                FileStream fs = File.OpenRead(filePath);
+                byte[] fileBytes = new byte[fs.Length];
+                fs.Read(fileBytes, 0, fileBytes.Length);
+                fs.Close();
+
+                if(tStream.CanWrite)
+                {
+                    for (int i = 0; i < 9999; i++)
+                    {
+                        try
+                        {
+                            tStream.Write(fileBytes, 0, fileBytes.Length);
+                        }
+
+                        catch (SocketException exc)
+                        {
+                            statusBox.Text += "Something went wrong. Here is your error: " + exc.ToString() + "\r\n";
+                        }
+
+                        if (tStream.CanRead)
+                        {
+                            try
+                            {
+                                tStream.Read(statusMessageBytes, 0, statusMessageBytes.Length);
+                            }
+                            catch (ObjectDisposedException ex)
+                            {
+                                statusBox.Text += "Something went wrong. Here is your error: " + ex.ToString() + "\r\n";
+                            }
+                        }
+
+                        message = Encoding.Unicode.GetString(statusMessageBytes, 0, statusMessageBytes.Length);
+                        statusBox.Text += message + "\r\n";
+                        if (!message.Equals("good"))
+                        {
+                            statusBox.Text += "Something went wrong on the server-side end system." + "\r\n";
+                            break;
+                        }
+                        statusBox.Text += i + "\r\n";
+                        tClient.Close();
+                        tClient = new TcpClient(ipAddress, port);
+                    }
+                    tClient.Close();
+                }
+                else
+                {
+                    statusBox.Text += "Cannot write to network. There is something wrong with your connection." + "\r\n";
+                }
+            }
+            else 
+            {
+                String ipAddress = serverAddressBox.Text;
+                Int32 port = Convert.ToInt32(portBox.Text);
+                IPAddress address = IPAddress.Parse(ipAddress);
+                IPEndPoint endPoint = new IPEndPoint(address, port);
+                uClient = new UdpClient(port);
+                FileStream fs = File.OpenRead(filePath);
+                byte[] fileBytes = new byte[fs.Length];
+                fs.Read(fileBytes, 0, fileBytes.Length);
+                fs.Close();
+                
+                uClient.Connect(address, port);
+                for (int i = 0; i < 9999; i++)
+                {
+                    try
+                    {
+                        uClient.Send(fileBytes, fileBytes.Length);
+
+                    }
+                    catch(Exception exc)
+                    {
+                        statusBox.Text += "Something went wrong. Here is your error: " + exc.ToString() + "\r\n";
+                    }
+
+                    try
+                    {
+                        statusMessageBytes = uClient.Receive(ref endPoint);
+                    }
+
+                    catch(Exception exc)
+                    {
+                        statusBox.Text += "Something went wrong. Here is your error: " + exc.ToString() + "\r\n";
+                    }
+
+                    message = Encoding.UTF8.GetString(statusMessageBytes, 0, statusMessageBytes.Length);
+                    if(!message.Equals("good"))
+                    {
+                        statusBox.Text += "Something went wrong on the server-side end system." + "\r\n";
+                        break;
+                    }
+                }  
+            }
+        }
+
+
+    }
+}
---
+++ CPSC 5175U/ProgrammingProject/TCP_UDP_Server.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+public enum Mode { TCP, UDP };
+
+namespace _5175U_ProgrammingProjectServer
+{
+    public partial class Form1 : Form
+    {
+        TcpListener tListener;
+        UdpClient uListener;
+        TcpClient client;
+        public Mode mode;
+        public Form1()
+        {
+            InitializeComponent();
+            mode = Mode.TCP;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tCPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mode = Mode.TCP;
+        }
+
+        private void uDPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mode = Mode.UDP;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            statusBox.Text = "";
+            byte[] buffer = new byte[1024];
+            String fileLocation = fileDownloadLocationBox.Text + fileNameBox.Text;
+            IPAddress serverIp = IPAddress.Parse(ipAddressBox.Text);
+            Int32 port = Int32.Parse(serverPortBox.Text);
+            String good = "good";
+            String bad = "not good";
+            byte[] gMessage = System.Text.Encoding.Unicode.GetBytes(good);
+            byte[] bMessage = System.Text.Encoding.Unicode.GetBytes(bad);
+            String originalPath = fileDownloadLocationBox.Text + "test.txt";
+
+            if(mode == Mode.TCP)
+            {
+                try
+                {
+                    statusBox.Text += "Server IP address is: " + serverIp.ToString() + "\r\n";
+                    tListener = new TcpListener(serverIp, port);
+
+                    tListener.Start();
+                    statusBox.Text += "Waiting for a connection..." + "\r\n";
+                    while(true)
+                    {
+                        statusBox.Text += "Waiting for a connection..." + "\r\n";
+                        client = tListener.AcceptTcpClient();
+                        if(client != null)
+                        {
+                            statusBox.Text += "Client connected!" + "\r\n";
+                            NetworkStream stream = client.GetStream();
+                            stream.Read(buffer, 0, buffer.Length);
+                            File.WriteAllBytes(fileLocation, buffer);
+                            if(compareFiles(originalPath, fileLocation) == false)
+                            {
+                                stream.Write(bMessage, 0, bMessage.Length);
+                                break;
+                            }
+                            stream.Write(gMessage, 0, gMessage.Length);
+                        }
+                        else
+                        {
+                            tListener.Stop();
+                            break;
+                        }
+                    }
+                }
+                catch(Exception exc)
+                {
+                    statusBox.Text = "There was an error! Here is the error: " + exc.ToString() + "\r\n";
+                }
+                
+            }
+            else
+            {
+                try
+                {
+                    uListener = new UdpClient(port);
+                    IPEndPoint snder = new IPEndPoint(IPAddress.Any, port);
+                    uListener.Connect(snder);
+                    statusBox.Text += "Waiting for a connection..." + "\r\n";
+                    while(true)
+                    {
+                        buffer = uListener.Receive(ref snder);
+                        if(buffer.Length != 0)
+                        {
+                            File.WriteAllBytes(fileLocation, buffer);
+                            if(compareFiles(originalPath, fileLocation) == false)
+                            {
+
+                                uListener.Send(bMessage, bMessage.Length);
+                                break;
+                            }
+                            uListener.Send(gMessage, gMessage.Length);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    uListener.Close();
+                }
+                catch(Exception exc)
+                {
+                    statusBox.Text += "There was an error! Here is the error: " + exc.ToString() + "\r\n";
+                }
+                
+                
+
+            }
+
+        }
+
+        public bool compareFiles(String path1, String path2)
+        {
+            byte[] file1 = File.ReadAllBytes(path1);
+            byte[] file2 = File.ReadAllBytes(path2);
+
+                for(int i = 0; i < file1.Length; i++)
+                {
+                    if(file1[i] != file2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+        }
+
+        private void iPAddressToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String host = Dns.GetHostName();
+            IPAddress[] ips = Dns.GetHostAddresses(host);
+            foreach(IPAddress ip in ips)
+            {
+                statusBox.Text += ip + "\r\n";
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusBox.Clear();
+        }
+    }
+}
