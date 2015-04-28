8831ba645d77df65e38ef76c937c3b6763bfd305
Nicolas Polhamus <polhamus.n@outlook.com>
2015-04-27 23:42:46 -0400

    Modular Inverse Tester
    Tester for a custom Modular Inverse function. Algorithm was pulled from
    the following link:
    http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers

---
+++ Personal Projects/ModInverseTester/ModInverseTester/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ModInverseTester
+{
+    partial class ModInverseTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.beginModInverseTest = new System.Windows.Forms.Button();
+            this.infoBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // beginModInverseTest
+            // 
+            this.beginModInverseTest.BackColor = System.Drawing.Color.Indigo;
+            this.beginModInverseTest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.beginModInverseTest.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginModInverseTest.Location = new System.Drawing.Point(90, 12);
+            this.beginModInverseTest.Name = "beginModInverseTest";
+            this.beginModInverseTest.Size = new System.Drawing.Size(75, 32);
+            this.beginModInverseTest.TabIndex = 0;
+            this.beginModInverseTest.Text = "Test";
+            this.beginModInverseTest.UseVisualStyleBackColor = false;
+            this.beginModInverseTest.Click += new System.EventHandler(this.beginModInverseTest_Click);
+            // 
+            // infoBox
+            // 
+            this.infoBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.infoBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.infoBox.Location = new System.Drawing.Point(12, 50);
+            this.infoBox.Multiline = true;
+            this.infoBox.Name = "infoBox";
+            this.infoBox.ReadOnly = true;
+            this.infoBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.infoBox.Size = new System.Drawing.Size(231, 147);
+            this.infoBox.TabIndex = 1;
+            // 
+            // ModInverseTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(255, 209);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.beginModInverseTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ModInverseTester";
+            this.Text = "Modular Inverse Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button beginModInverseTest;
+        private System.Windows.Forms.TextBox infoBox;
+    }
+}
+
---
+++ Personal Projects/ModInverseTester/ModInverseTester/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace ModInverseTester
+{
+    public partial class ModInverseTester : Form
+    {
+        public ModInverseTester()
+        {
+            InitializeComponent();
+        }
+
+        private void beginModInverseTest_Click(object sender, EventArgs e)
+        {
+            BigInteger a = new BigInteger(probablePrime());
+            BigInteger b = new BigInteger(probablePrime());
+            while(true)
+            {
+                if(!a.Equals(b))
+                {
+                    break;
+                }
+                a = new BigInteger(probablePrime());
+                b = new BigInteger(probablePrime());
+            }
+            BigInteger c = inverse(a,b);
+            if (c.Equals(BigInteger.Zero))
+            {
+                infoBox.Text += c.ToString() + " is not an inverse of " + a.ToString() + " when mod by " + b.ToString() + "\r\n" +
+                    "##############################" + "\r\n";
+            }
+            else
+            {
+                infoBox.Text += c.ToString() + " is an inverse of " + a.ToString() + " when mod by " + b.ToString() + "\r\n" + 
+                    "##############################" + "\r\n";
+            }
+
+        }
+
+        #region Inverse Modulo function derived from the psuedocode available @http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
+        public static BigInteger inverse(BigInteger a, BigInteger b)
+        {
+            BigInteger r = b;
+            BigInteger new_r = a;
+            BigInteger quotient = new BigInteger();
+            BigInteger t = new BigInteger(0);
+            BigInteger new_t = new BigInteger(1);
+            BigInteger temp = new BigInteger();
+            while (new_r != BigInteger.Zero)
+            {
+                quotient = BigInteger.Divide(r, new_r);
+                temp = new_t;
+                new_t = BigInteger.Subtract(t, BigInteger.Multiply(quotient, new_t));
+                t = temp;
+                temp = new_r;
+                new_r = BigInteger.Subtract(r, BigInteger.Multiply(quotient, new_r));
+                r = temp;
+                
+            }
+
+            if (r > 1)
+            {
+                t = BigInteger.Zero;
+                return t;
+            }
+            if (t < 0)
+            {
+                t = BigInteger.Add(t, b);
+            }
+            return t;
+        }
+        #endregion
+
+        #region Probable Prime method
+        private double probablePrime()
+        {
+            Random rand = new Random();
+            double n = 0;
+            while (true)
+            {
+                n = rand.Next(0,9999);
+                if (IsPrime(n))
+                {
+                    infoBox.Text += n + " is prime." + "\r\n";
+                    break;
+                }
+                else
+                {
+                    infoBox.Text += n + " is not prime." + "\r\n";
+                }
+            }
+            return n;
+        }
+        #endregion
+
+        #region Primality Test from Wikipedia, url@http://en.wikipedia.org/wiki/Primality_test
+        public static bool IsPrime(double n)
+        {
+            if (n <= 3)
+            {
+                return n > 1;
+            }
+            else if (n % 2 == 0 || n % 3 == 0)
+            {
+                return false;
+            }
+            for (ulong i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+    }
+}
